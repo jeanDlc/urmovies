7c91cc4a094750a8000be69eab1697e67ddd9460
Jean Pierre <jeanpierre101298@gmail.com>
2023-11-17 14:46:33 -0500

    Enhancement / Use service helper to request home page data

--- services/index.ts
+++ services/index.ts
@@ -10,3 +10,18 @@ export const getRankedMovies = async () => {
 
   return data.results as Movie[];
 };
+
+export const getPopularMovies = async () => {
+  const url = Api.buildRequestUrl({ path: "/movie/popular" });
+
+  const res = await fetch(url);
+
+  const data: {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+  } = await res.json();
+
+  return data;
+};
